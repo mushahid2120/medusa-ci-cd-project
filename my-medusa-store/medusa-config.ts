54d0ff061c5635ef470cc718d428f75538f3059a
Md Mushahid Ansari <142341806+mushahid2120@users.noreply.github.com>
2025-06-01 22:23:50 +0530

    Update medusa-config.ts

--- my-medusa-store/medusa-config.ts
+++ my-medusa-store/medusa-config.ts
@@ -4,12 +4,12 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
+          databaseUrl: "postgres://$(DATABASE_USERNAME):$(DATABASE_PASSWORD)@$(DATABASE_URL):5432/my-medusa?sslmode=require&rejectUnauthorized=false",
+        redisUrl: "rediss://$(REDIS_URL):6379",
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || "http://localhost:8000",
+      adminCors: process.env.ADMIN_CORS || "http://localhost:9000",
+      authCors: process.env.AUTH_CORS || "http://localhost:8000,http://localhost:9000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
